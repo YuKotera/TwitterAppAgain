763338e97df960fb177d2cdaca05a3f6cbaa5b75
Yu kotera <koterayu@gmail.com>
2015-09-19 18:31:50 +0900

    Twitterにコメント投稿できるようになった。投稿するところまるコピー

--- TwitterAppAgain/TimeLineTableViewController.swift
+++ TwitterAppAgain/TimeLineTableViewController.swift
@@ -7,10 +7,18 @@
 //
 
 import UIKit
+import Social
 
 class TimeLineTableViewController: UITableViewController {
     
-    let dataArray:[[String:String]] = [["title":"タイトル1"],["title":"タイトル2"]]
+    let dataArray:[[String:String]] = [
+        [  "title" : "タイトル1",
+           "image" :"https://pbs.twimg.com/profile_images/378800000220029324/fe66faeca20115da8566e51d83447ead.jpeg"
+        ],
+        [   "title": "タイトル2",
+            "image": "https://pbs.twimg.com/profile_images/496137097007218688/B5XJEdE2.jpeg"
+        ]
+    ]
     
     //テーブルの件数を登録
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -24,8 +32,47 @@ class TimeLineTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath) as! TimeLineTableViewCell
         
         cell.tweetLabel.text = dataArray[indexPath.row]["title"]
+        if let imageURLString = dataArray[indexPath.row]["image"]
+            ,let imageURL = NSURL(string: imageURLString){
+                
+        
+                cell.iconimageView.sd_setImageWithURL(imageURL)
+        }
         
         return cell
+        
     }
+    
+    
+    @IBAction func taTweetpButton(sender: UIBarButtonItem) {
+        if SLComposeViewController.isAvailableForServiceType(SLServiceTypeTwitter) {
+            
+            let vc = SLComposeViewController(forServiceType: SLServiceTypeTwitter)
+            
+            //CancelもしくはPostを押した際に呼ばれ、投稿画面を閉じる処理を行っています。
+            vc.completionHandler = {(result:SLComposeViewControllerResult) -> () in
+                vc.dismissViewControllerAnimated(true, completion:nil)
+            }
+            
+            ////投稿画面の初期値設定
+            //vc.setInitialText("初期テキストを設定できます。")
+            //vc.addURL(NSURL(string:"シェアURLを設定できます。"))
+            self.presentViewController(vc, animated: true, completion: nil)
+            
+        }else{
+            let alert = UIAlertController(title: "エラー", message: "Twitterアカウントが登録されていません。設定アプリを開きますか？", preferredStyle: UIAlertControllerStyle.Alert)
+            
+            alert.addAction(UIAlertAction(title: "はい", style: UIAlertActionStyle.Default, handler: { (action) -> Void in
+                if let URL = NSURL(string: UIApplicationOpenSettingsURLString){
+                    UIApplication.sharedApplication().openURL(URL)
+                }
+            }))
+            
+            alert.addAction(UIAlertAction(title: "いいえ", style: UIAlertActionStyle.Default, handler:nil))
+            self.presentViewController(alert, animated: true, completion: nil)
+        }
+    }
+    
+    
     
 }
---
+++ hoge.m
@@ -0,0 +1,9 @@
+//
+//  hoge.m
+//  TwitterAppAgain
+//
+//  Created by yukotera on 2015/09/19.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
