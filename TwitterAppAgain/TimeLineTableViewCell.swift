bf8fe014350934cef78a05e6d434bb2ed2cb1d79
Yu kotera <koterayu@gmail.com>
2015-09-19 17:12:51 +0900

    リネーム

---
+++ TwitterAppAgain/TimeLineTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TimeLineTableViewCell.swift
+//  TwitterAppAgain
+//
+//  Created by yukotera on 2015/09/19.
+//  Copyright (c) 2015年 myname. All rights reserved.
+//
+
+import UIKit
+
+class TimeLineTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
